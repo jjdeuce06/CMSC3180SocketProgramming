db4cf3403c24824c888792d20dc62a1a6795cb1c
John Gerega <jjgerega@gmail.com>
2025-09-24 15:52:11 -0400

    Code from slides put into repo

---
+++ ExampleSocket.py
@@ -0,0 +1,25 @@
+from socket import *
+
+#Server
+serverName = 'serverName' # I assume this will be Draco?
+serverPort = 38100
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From Server:', modifiedSentence.decode())
+clientSocket.close()
+
+#Client
+clientPort = 38100
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', clientPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.close()
