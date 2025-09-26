d5ea50ea311b1bed5967e49907db5470fb78e549
John Gerega <jjgerega@gmail.com>
2025-09-26 16:02:21 -0400

    Established a connection I think???

--- Client.py
+++ Client.py
@@ -5,15 +5,16 @@ clientInput = int(input("Enter a port number: "))
 
 try:
     clientPort = clientInput
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('', clientPort))
-    serverSocket.listen(10)
-    print("The server is ready to receive")
-    while True:
-        connectionSocket, addr = serverSocket.accept()
-        sentence = connectionSocket.recv(1024).decode()
-        capitalizedSentence = sentence.upper()
-        connectionSocket.send(capitalizedSentence.encode())
-        connectionSocket.close()
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(('158.83.11.22', clientPort))
+    print("Connection established.")
+    #while True:
+    #connectionSocket, addr = clientSocket.accept()
+        #print("Connected to:", addr)
+        #sentence = connectionSocket.recv(1024).decode()
+        #capitalizedSentence = sentence.upper()
+        #connectionSocket.send(capitalizedSentence.encode())
+    clientSocket.send("Hello")
+    clientSocket.close()
 except Exception as e:
     print("An error occurred:", e)
--- Server.py
+++ Server.py
@@ -1,15 +1,14 @@
 from socket import *
 
 
-
-#Server
-#serverName = 'DRACO1' # I assume this will be Draco?
-serverPort = 38100
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(("158.83.11.22", serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From Server:', modifiedSentence.decode())
-clientSocket.close()
+serverPort = 31800
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('158.83.11.22', serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+#while True:
+connectionSocket, addr = serverSocket.accept()
+print("Connected to:", addr)
+connectionSocket.send("Hello".encode())
+connectionSocket.close()
 
