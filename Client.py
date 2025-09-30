5a599666147bb42e256bcd68ad5b98408073f390
John Gerega <jjgerega@gmail.com>
2025-09-30 11:06:44 -0400

    Comments

--- Client.py
+++ Client.py
@@ -1,43 +1,50 @@
 # Import socket module 
-import socket             
+import socket            
 
 # Create a socket object 
 s = socket.socket()         
-
+print("----------------Client----------------")
 while True:
-    # Define the port on which you want to connect 
+    # Ask user for the port
     port = int(input("Enter port number: ")) 
 
-    # connect to the server on local computer 
+    # attempt connect to the server
     try:
         s.connect(('158.83.11.22', port)) 
         print("Connection established")
         serverGreeting = s.recv(1024)
         print("From Server:", serverGreeting.decode())
         break
+    #print an error message if no connection can be made
     except ConnectionRefusedError:
         print("Connection refused. Make sure the port number is correct.")
 
+# loop to send and receive data
 while True:
-    sentence = input("Enter a sentence: ")
+    #ask user for a sentence
+    sentence = input("Enter a sentence (exit to quit): ")
+    #if the user enters exit, close the connection and exit the loop
     if sentence.lower() == 'exit':
         print("Exiting...")
         capitalizedSentence = sentence.upper()
         s.send(capitalizedSentence.encode())
         break
+    #if user enters anything else, prompt for echo option
     echo = input("Server echo? (y/n): ")
+    #if user enters n, send the sentence and reprompt
     if echo.lower() == 'n':
         capitalizedSentence = sentence.upper()
         s.send(capitalizedSentence.encode())
-        #s.send(echo.encode())
         modifiedSentence = s.recv(1024)
+    #if the user enters y, send the sentence, receive the capitalized sentence back, and print it
     elif echo.lower() == 'y':
         capitalizedSentence = sentence.upper()
         s.send(capitalizedSentence.encode())
-        #s.send(echo.encode())
         modifiedSentence = s.recv(1024)
         print("From Server:", modifiedSentence.decode())
+    #if user enters anything else, restart prompt from the sentence
     else:
         print("Invalid input. Please enter 'y' or 'n'.")
 
+#close the socket
 s.close() 
