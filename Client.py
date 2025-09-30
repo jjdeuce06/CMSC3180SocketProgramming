8c65b836c4eab6973c3de2ccf085cd317cb1c4e1
Luke Ruffing <luketruffing@gmail.com>
2025-09-30 12:36:50 -0400

    Changed the echo toggle system

--- Client.py
+++ Client.py
@@ -2,11 +2,12 @@
 import socket            
 
 # Create a socket object 
-s = socket.socket()         
+s = socket.socket()       
 print("----------------Client----------------")
 while True:
     # Ask user for the port
     port = int(input("Enter port number: ")) 
+    echo = 1
 
     # attempt connect to the server
     try:
@@ -19,32 +20,44 @@ while True:
     except ConnectionRefusedError:
         print("Connection refused. Make sure the port number is correct.")
 
+
+print("---------------Commands---------------\n1. :echo off\n2. :echo on\n3. :help\n4. :exit")
 # loop to send and receive data
 while True:
+    
     #ask user for a sentence
-    sentence = input("Enter a sentence (exit to quit): ")
+    sentence = input("Enter a sentence (:exit to quit): ")
     #if the user enters exit, close the connection and exit the loop
-    if sentence.lower() == 'exit':
+    if sentence.lower() == ':exit':
         print("Exiting...")
-        capitalizedSentence = sentence.upper()
-        s.send(capitalizedSentence.encode())
         break
-    #if user enters anything else, prompt for echo option
-    echo = input("Server echo? (y/n): ")
-    #if user enters n, send the sentence and reprompt
-    if echo.lower() == 'n':
-        capitalizedSentence = sentence.upper()
-        s.send(capitalizedSentence.encode())
-        modifiedSentence = s.recv(1024)
-    #if the user enters y, send the sentence, receive the capitalized sentence back, and print it
-    elif echo.lower() == 'y':
+    
+    if sentence.lower() == ':help':
+        print("---------------Commands---------------\n1. :echo off\n2. :echo on\n3. :help\n4. :exit")
+        continue
+    
+    #echo control
+    if sentence.lower() == ':echo off':
+        echo = 0
+        continue
+            
+    if sentence.lower() == ':echo on':
+        echo = 1
+        continue
+    
+    
+    #print control  
+    if echo == 1:
         capitalizedSentence = sentence.upper()
         s.send(capitalizedSentence.encode())
         modifiedSentence = s.recv(1024)
         print("From Server:", modifiedSentence.decode())
-    #if user enters anything else, restart prompt from the sentence
     else:
-        print("Invalid input. Please enter 'y' or 'n'.")
+        capitalizedSentence = sentence.upper()
+        s.send(capitalizedSentence.encode())
+        #recieves the message but does not save it
+        s.recv(1024)
+        continue
 
 #close the socket
 s.close() 
