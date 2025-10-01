19a50f6be772f988084d41e823ef994e71bf3bb3
John Gerega <jjgerega@gmail.com>
2025-10-01 15:45:54 -0400

    Fixed it up, user has to quit twice to actually quit program.

--- Client.py
+++ Client.py
@@ -2,62 +2,60 @@
 import socket            
 
 # Create a socket object 
-s = socket.socket()       
+       
 print("----------------Client----------------")
 while True:
     # Ask user for the port
-    port = int(input("Enter port number: ")) 
-    echo = 1
-
+    s = socket.socket()  
+    port_input = input("Enter port number (quit to exit): ")
+    if port_input.lower() == 'quit':
+        print("Exiting Program...")
+        break
+    if not port_input.isdigit():
+        print("Invalid input. Please enter a valid port number.")
+        continue
     # attempt connect to the server
     try:
+        port = int(port_input)
         s.connect(('158.83.11.22', port)) 
         print("Connection established")
         serverGreeting = s.recv(1024)
         print("From Server:", serverGreeting.decode())
-        break
+        # loop to send and receive data
+        while True:
+            #ask user for a sentence
+            sentence = input("Enter a sentence (exit to quit): ")
+            #if the user enters exit, close the connection and exit the loop
+            if sentence.lower() == 'exit':
+                print("Exiting Connection...")
+                capitalizedSentence = sentence.upper()
+                s.send(capitalizedSentence.encode())
+                #close the socket
+                s.close() 
+                break
+            #if user enters anything else, prompt for echo option
+            echo = input("Server echo? (y/n): ")
+            #if user enters n, send the sentence and reprompt
+            if echo.lower() == 'n':
+                capitalizedSentence = sentence.upper()
+                s.send(capitalizedSentence.encode())
+                modifiedSentence = s.recv(1024)
+            #if the user enters y, send the sentence, receive the capitalized sentence back, and print it
+            elif echo.lower() == 'y':
+                capitalizedSentence = sentence.upper()
+                s.send(capitalizedSentence.encode())
+                modifiedSentence = s.recv(1024)
+                print("From Server:", modifiedSentence.decode())
+            #if user enters anything else, restart prompt from the sentence
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
     #print an error message if no connection can be made
     except ConnectionRefusedError:
         print("Connection refused. Make sure the port number is correct.")
+    except OverflowError:
+        print("Port number must be in the range 0-65535.")
 
-
-print("---------------Commands---------------\n1. :echo off\n2. :echo on\n3. :help\n4. :exit")
-# loop to send and receive data
-while True:
     
-    #ask user for a sentence
-    sentence = input("Enter a sentence (:exit to quit): ")
-    #if the user enters exit, close the connection and exit the loop
-    if sentence.lower() == ':exit':
-        print("Exiting...")
-        break
-    
-    if sentence.lower() == ':help':
-        print("---------------Commands---------------\n1. :echo off\n2. :echo on\n3. :help\n4. :exit")
-        continue
-    
-    #echo control
-    if sentence.lower() == ':echo off':
-        echo = 0
-        continue
-            
-    if sentence.lower() == ':echo on':
-        echo = 1
-        continue
-    
-    
-    #print control  
-    if echo == 1:
-        capitalizedSentence = sentence.upper()
-        s.send(capitalizedSentence.encode())
-        modifiedSentence = s.recv(1024)
-        print("From Server:", modifiedSentence.decode())
-    else:
-        capitalizedSentence = sentence.upper()
-        s.send(capitalizedSentence.encode())
-        #recieves the message but does not save it
-        s.recv(1024)
-        continue
 
 #close the socket
 s.close() 
