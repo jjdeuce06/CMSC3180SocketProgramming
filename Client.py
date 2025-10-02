613a6e6db79a5931042cedc74627b2ba4731a0f9
Luke Ruffing <luketruffing@gmail.com>
2025-10-01 20:04:39 -0400

    added toggle echo and new port commands

--- Client.py
+++ Client.py
@@ -2,13 +2,14 @@
 import socket            
 
 # Create a socket object 
-       
+run = True  
 print("----------------Client----------------")
-while True:
+while run:
     # Ask user for the port
-    s = socket.socket()  
-    port_input = input("Enter port number (quit to exit): ")
-    if port_input.lower() == 'quit':
+    s = socket.socket() 
+    echo = 1 
+    port_input = input("Enter port number (:exit to quit): ")
+    if port_input.lower() == ':exit':
         print("Exiting Program...")
         break
     if not port_input.isdigit():
@@ -21,34 +22,60 @@ while True:
         print("Connection established")
         serverGreeting = s.recv(1024)
         print("From Server:", serverGreeting.decode())
+        print("---------------Commands---------------\n1. :help\n2. :echo off\n3. :echo on\n4. :new port\n5. :exit")
         # loop to send and receive data
         while True:
             #ask user for a sentence
-            sentence = input("Enter a sentence (exit to quit): ")
+            sentence = input("Enter a sentence (:exit to quit): ")
+
+            #if the user enters nothing, reprompt to stop breakages
+            if sentence == '':
+                print("Please enter a valid sentence.")
+                continue
+
             #if the user enters exit, close the connection and exit the loop
-            if sentence.lower() == 'exit':
+            if sentence.lower() == ':exit':
                 print("Exiting Connection...")
                 capitalizedSentence = sentence.upper()
-                s.send(capitalizedSentence.encode())
+                s.send(capitalizedSentence.encode()) 
+                run = False
                 #close the socket
                 s.close() 
                 break
-            #if user enters anything else, prompt for echo option
-            echo = input("Server echo? (y/n): ")
+            
+            if sentence.lower() == ':new port':
+                print("Allowing New Port...")
+                #close the socket
+                s.close() 
+                break
+            
+            if sentence.lower() == ':help':
+                print("---------------Commands---------------\n1. :help\n2. :echo off\n3. :echo on\n4. :new port\n5. :exit")
+                continue
+
+            if sentence.lower() == ':echo off':
+                echo = 0
+                continue
+        
+            if sentence.lower() == ':echo on':
+                echo = 1
+                continue
+
             #if user enters n, send the sentence and reprompt
-            if echo.lower() == 'n':
+            if echo == 0:
                 capitalizedSentence = sentence.upper()
                 s.send(capitalizedSentence.encode())
                 modifiedSentence = s.recv(1024)
             #if the user enters y, send the sentence, receive the capitalized sentence back, and print it
-            elif echo.lower() == 'y':
+            elif echo == 1:
                 capitalizedSentence = sentence.upper()
                 s.send(capitalizedSentence.encode())
                 modifiedSentence = s.recv(1024)
                 print("From Server:", modifiedSentence.decode())
             #if user enters anything else, restart prompt from the sentence
             else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+                print("Invalid input. Please enter :echo on or :echo off.")
+                continue
     #print an error message if no connection can be made
     except ConnectionRefusedError:
         print("Connection refused. Make sure the port number is correct.")
